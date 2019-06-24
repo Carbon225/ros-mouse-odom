5713f13cec401b202dbad0cc8867d5fdbea15b8b
Carbon225 <carbon.szlachta@gmail.com>
2019-06-24 06:42:21 -0700

    New math

--- src/odom_combiner_node.cpp
+++ src/odom_combiner_node.cpp
@@ -2,8 +2,11 @@
 
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/TransformStamped.h>
+
+#include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Transform.h>
+
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
 
@@ -11,7 +14,7 @@
 #define DIRECTED_ANGLE(vector1, vector2) atan2(vector2.getY(), vector2.getX()) - atan2(vector1.getY(), vector1.getX())
 
 
-void odomCallback(const nav_msgs::Odometry odom);
+void odomCallback(const nav_msgs::Odometry trackedPoint);
 
 tf2_ros::TransformBroadcaster *transformBroadcaster_ptr;
 tf2_ros::Buffer tfBuffer;
@@ -32,54 +35,98 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void odomCallback(const nav_msgs::Odometry odom)
+void odomCallback(const nav_msgs::Odometry trackedPoint)
 {
     geometry_msgs::TransformStamped mouseTransform;
+    geometry_msgs::TransformStamped odomTransform;
     try {
-        mouseTransform = tfBuffer.lookupTransform("base_link", odom.header.frame_id, ros::Time(0));
+        mouseTransform = tfBuffer.lookupTransform("base_link", trackedPoint.header.frame_id, ros::Time(0));
     }
     catch (tf2::TransformException &ex) {
         ROS_WARN("%s",ex.what());
         return;
-        // no movement on exception
-        /*odom.pose.pose.position.x = 0.f;
-        odom.pose.pose.position.y = 0.f;
-        odom.pose.pose.position.z = 0.f;*/
     }
 
-    double alpha = 0.f; // base_link rotation after translation
-    tf2::Vector3 P;
-    tf2::Vector3 X;
+    try {
+        odomTransform = tfBuffer.lookupTransform("base_link", trackedPoint.child_frame_id, ros::Time(0));
+    }
+    catch (tf2::TransformException &ex) {
+        ROS_WARN("%s",ex.what());
 
-    // mouse -> odom translation
-    X.setX(odom.pose.pose.position.x);
-    X.setY(odom.pose.pose.position.y);
+        // send default odom -> base_link transform
+        geometry_msgs::TransformStamped odom_trans;
+        odom_trans.header.stamp = ros::Time::now();
+        odom_trans.header.frame_id = trackedPoint.child_frame_id;
+        odom_trans.child_frame_id = "base_link";
+
+        odom_trans.transform.rotation.w = 1.f;
+
+        transformBroadcaster_ptr->sendTransform(odom_trans);
+
+        return;
+    }
+
+    double alphaT = 0.f;
+    double alphaC = 0.f;
+    double alphaO = 0.f;
+    tf2::Vector3 P, X, O, T, A, B, C;
+
+    // mouse -> tracked odom point
+    X.setX(trackedPoint.pose.pose.position.x);
+    X.setY(trackedPoint.pose.pose.position.y);
     ROS_DEBUG("Xx = %g Xy = %g", X.getX(), X.getY());
 
-    // mouse position
+    // base_link -> mouse
     P.setX(mouseTransform.transform.translation.x);
     P.setY(mouseTransform.transform.translation.y);
     ROS_DEBUG("Px = %g Py = %g", P.getX(), P.getY());
 
-    // alpha = tf2::tf2Angle(P, P + X);
-    alpha = DIRECTED_ANGLE(P, (P + X));
-    ROS_DEBUG("a = %g", alpha);
+    // base_link -> odom
+    {
+        tf2::Quaternion odom_quat;
+        odom_quat.setX(odomTransform.transform.rotation.x);
+        odom_quat.setY(odomTransform.transform.rotation.y);
+        odom_quat.setZ(odomTransform.transform.rotation.z);
+        odom_quat.setW(odomTransform.transform.rotation.w);
+
+        double roll, pitch, yaw;
+        tf2::Matrix3x3(odom_quat).getRPY(roll, pitch, yaw);
+
+        O.setX(odomTransform.transform.translation.x);
+        O.setY(odomTransform.transform.translation.y);
+        alphaO = yaw;
+
+        ROS_DEBUG("Ox = %g Oy = %g Oa = %g", O.getX(), O.getY(), alphaO);
+    }
+
+    // rotate mouse pos to odom frame
+    A.setX( cos(alphaO)*P.getX() - sin(alphaO)*P.getY() );
+    A.setY( sin(alphaO)*P.getX() + cos(alphaO)*P.getY() );
+
+    // mouse in odom frame -> tracked point
+    B = X - O - A;
+
+    // odom -> new odom
+    alphaC = DIRECTED_ANGLE(A, (A + B));
+
+    tf2::Vector3 rotA;
+    rotA.setX( cos(alphaC)*A.getX() - sin(alphaC)*A.getY() );
+    rotA.setY( sin(alphaC)*A.getX() + cos(alphaC)*A.getY() );
+
+    C = A + B - rotA;
+
+    // base_link -> new odom
+    alphaT = alphaO + alphaC;
+    T = O + C;
 
     // transform base_link -> odom
     tf2::Transform tf_transform;
 
-    // rotation component
     tf2::Quaternion tf_quat;
-    tf_quat.setRPY(0.f, 0.f, alpha);
+    tf_quat.setRPY(0.f, 0.f, alphaT);
 
     tf_transform.setRotation(tf_quat);
-
-    // translation component
-    tf2::Vector3 rotP;
-    rotP.setX( cos(alpha)*P.getX() - sin(alpha)*P.getY() );
-    rotP.setY( sin(alpha)*P.getX() + cos(alpha)*P.getY() );
-
-    tf_transform.setOrigin(P + X - rotP);
+    tf_transform.setOrigin(T);
 
     ROS_DEBUG("Tx = %g Ty = %g", tf_transform.getOrigin().getX(), tf_transform.getOrigin().getY());
 
@@ -89,12 +136,12 @@ void odomCallback(const nav_msgs::Odometry odom)
     // compose transform message
     geometry_msgs::TransformStamped odom_trans;
     odom_trans.header.stamp = ros::Time::now();
-    odom_trans.header.frame_id = "odom";
+    odom_trans.header.frame_id = trackedPoint.child_frame_id;
     odom_trans.child_frame_id = "base_link";
 
     odom_trans.transform.translation.x = tf_transform.getOrigin().x();
     odom_trans.transform.translation.y = tf_transform.getOrigin().y();
-    odom_trans.transform.translation.z = tf_transform.getOrigin().z();
+    // odom_trans.transform.translation.z = tf_transform.getOrigin().z();
 
     odom_trans.transform.rotation.x = tf_transform.getRotation().x();
     odom_trans.transform.rotation.y = tf_transform.getRotation().y();
