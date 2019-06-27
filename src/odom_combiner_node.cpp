5178881e59e7db68864f856ea1f7f5332a93e6a8
Carbon225 <carbon.szlachta@gmail.com>
2019-06-26 18:47:04 -0700

    Locally keep track of odom

--- src/odom_combiner_node.cpp
+++ src/odom_combiner_node.cpp
@@ -22,6 +22,10 @@ tf2::Vector3 point1, point2;
 tf2::Vector3 last_point1, last_point2;
 
 
+tf2::Vector3 base_link_v(0.f, 0.f, 0.f);
+double base_link_a = 0.f;
+
+
 tf2_ros::TransformBroadcaster *transformBroadcaster_ptr;
 tf2_ros::Buffer tfBuffer;
 
@@ -40,26 +44,25 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    ros::Subscriber odom1Sub = nh.subscribe(mouse1_frame + "/point", 256, odom1Callback);
-    ros::Subscriber odom2Sub = nh.subscribe(mouse2_frame + "/point", 256, odom2Callback);
+    ros::Subscriber odom1Sub = nh.subscribe(mouse1_frame + "/odom", 256, odom1Callback);
+    ros::Subscriber odom2Sub = nh.subscribe(mouse2_frame + "/odom", 256, odom2Callback);
+
+    ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>("odom_out", 256);
 
     tf2_ros::TransformBroadcaster transformBroadcaster;
     transformBroadcaster_ptr = &transformBroadcaster;
 
     tf2_ros::TransformListener tfListener(tfBuffer);
 
-
     while (ros::ok()) {
         // calculate relative point movement
-        tf2::Vector3 X1(
-                    point1 - last_point1
-                );
+        tf2::Vector3 X1 = point1 - last_point1;
         last_point1 = point1;
 
-        tf2::Vector3 X2(
-                point2 - last_point2
-        );
+        tf2::Vector3 X2 = point2 - last_point2;
         last_point2 = point2;
+
+        ros::Time current_time = ros::Time::now();
 
         // get mouse positions
         tf2::Vector3 mouse1p, mouse2p;
@@ -68,7 +71,7 @@ int main(int argc, char **argv) {
                     tfBuffer.lookupTransform("base_link", mouse1_frame, ros::Time(0));
 
             geometry_msgs::TransformStamped mouse2Transform =
-                    tfBuffer.lookupTransform("base_link", mouse1_frame, ros::Time(0));
+                    tfBuffer.lookupTransform("base_link", mouse2_frame, ros::Time(0));
 
             mouse1p.setX( mouse1Transform.transform.translation.x );
             mouse1p.setY( mouse1Transform.transform.translation.y );
@@ -83,6 +86,7 @@ int main(int argc, char **argv) {
 
         // from point 1 to point 2 in base_link frame
         tf2::Vector3 X1X2 = (mouse2p + X2) - (mouse1p + X1);
+        // from mouse 1 to mouse 2
         tf2::Vector3 mouse1mouse2 = mouse2p - mouse1p;
 
         // base_link rotation angle
@@ -91,72 +95,44 @@ int main(int argc, char **argv) {
         // point between 2 points
         tf2::Vector3 pointAverage = ((mouse1p + X1) + (mouse2p + X2)) / 2.f;
 
-        // odom -> base_link relative transform
-        tf2::Transform base_link_tf;
-        base_link_tf.setIdentity();
-        base_link_tf.setOrigin(pointAverage);
-        {
-            tf2::Quaternion q;
-            q.setRPY(0.f, 0.f, alpha);
-            base_link_tf.setRotation(q);
-        }
+        // move base_link by pointAverage in base_link frame
+        base_link_v += pointAverage.rotate(tf2::Vector3(0.f, 0.f, 1.f), base_link_a);
 
-        // get base_link transform
-        tf2::Transform current_base_link_tf;
-        try {
-            // get wheel position from transform
-            geometry_msgs::TransformStamped baseTransform =
-                    tfBuffer.lookupTransform("odom", "base_link", ros::Time(0));
-
-            tf2::Vector3 v;
-            v.setX( baseTransform.transform.translation.x );
-            v.setY( baseTransform.transform.translation.y );
-            v.setZ( baseTransform.transform.translation.z );
-            current_base_link_tf.setOrigin(v);
-
-            tf2::Quaternion q;
-            q.setX( baseTransform.transform.rotation.x );
-            q.setY( baseTransform.transform.rotation.y );
-            q.setZ( baseTransform.transform.rotation.z );
-            q.setW( baseTransform.transform.rotation.w );
-            current_base_link_tf.setRotation(q);
-        }
-        catch (tf2::TransformException &ex) {
-            ROS_WARN("%s",ex.what());
-
-            current_base_link_tf.setIdentity();
-        }
-
-        tf2::Transform new_base_link_tf;
-        new_base_link_tf.setIdentity();
-
-        // new = current + local movement rotated into global frame
-        new_base_link_tf.setOrigin(
-                current_base_link_tf.getOrigin() +
-                base_link_tf.getOrigin().rotate(
-                        base_link_tf.getRotation().getAxis(), base_link_tf.getRotation().getAngle())
-                );
-        new_base_link_tf.setRotation(current_base_link_tf.getRotation() * base_link_tf.getRotation());
-
-
-        // new_base_link_tf.setOrigin(current_base_link_tf.getOrigin() + base_link_tf.getOrigin());
-
+        base_link_a += alpha;
 
         // compose transform message
         geometry_msgs::TransformStamped odom_trans;
-        odom_trans.header.stamp = ros::Time::now();
+        odom_trans.header.stamp = current_time;
         odom_trans.header.frame_id = "odom";
         odom_trans.child_frame_id = "base_link";
 
-        odom_trans.transform.translation.x = new_base_link_tf.getOrigin().x();
-        odom_trans.transform.translation.y = new_base_link_tf.getOrigin().y();
+        odom_trans.transform.translation.x = base_link_v.x();
+        odom_trans.transform.translation.y = base_link_v.y();
 
-        odom_trans.transform.rotation.x = new_base_link_tf.getRotation().x();
-        odom_trans.transform.rotation.y = new_base_link_tf.getRotation().y();
-        odom_trans.transform.rotation.z = new_base_link_tf.getRotation().z();
-        odom_trans.transform.rotation.w = new_base_link_tf.getRotation().w();
+        tf2::Quaternion q;
+        q.setRPY(0.f, 0.f, base_link_a);
+
+        odom_trans.transform.rotation.x = q.x();
+        odom_trans.transform.rotation.y = q.y();
+        odom_trans.transform.rotation.z = q.z();
+        odom_trans.transform.rotation.w = q.w();
 
         transformBroadcaster_ptr->sendTransform(odom_trans);
+
+        nav_msgs::Odometry odom_msg;
+        odom_msg.header.stamp = current_time;
+        odom_msg.header.frame_id = "odom";
+        odom_msg.child_frame_id = "base_link";
+
+        odom_msg.pose.pose.position.x = base_link_v.x();
+        odom_msg.pose.pose.position.y = base_link_v.y();
+
+        odom_msg.pose.pose.orientation.x = q.x();
+        odom_msg.pose.pose.orientation.y = q.y();
+        odom_msg.pose.pose.orientation.z = q.z();
+        odom_msg.pose.pose.orientation.w = q.w();
+
+        odom_pub.publish(odom_msg);
 
         ros::spinOnce();
     }
